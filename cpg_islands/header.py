8a6c8f042f5f4c1faea368004fce9ee5a656c597
Sean Fisk <sean@seanfisk.com>
2012-04-18 13:29:56 -0400

    Proper documentation for header module.

--- cpg_islands/header.py
+++ cpg_islands/header.py
@@ -1,3 +1,14 @@
+""":mod:`cpg_islands.header` --- Helper module to print a nice-looking header
+"""
+
 def header(header_text, char='-'):
-    """Print text from a header and a line of header characters below it."""
+    """Print text from a header and a line of header characters below it.
+
+    :param header_text: the text to print
+    :type header_text: :class:`str`
+    :param char: the header character
+    :type char: :class:`str`
+    :return: the formatted header
+    :rtype: :class:`str`
+    """
     return header_text + '\n' + len(header_text) * char
